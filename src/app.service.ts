5e28f9dd0e11bac599c18469d452264b0c0a7482
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-11 12:50:20 +0700

    feat: update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,12 @@
 // app.service.ts
 import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { ClientKafka } from '@nestjs/microservices';
-import { firstValueFrom } from 'rxjs';
+import { catchError, firstValueFrom, lastValueFrom, of, timeout } from 'rxjs';
 import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class AppService implements OnModuleInit {
   private readonly requestTopic = 'test.request.topic';
-  private readonly replyTopic = 'test.reply.topic'; // ✅ ใช้ topic ที่ register ไว้แล้ว
 
   constructor(
     @Inject('KAFKA_CLIENT') private readonly kafka: ClientKafka,
@@ -15,27 +14,23 @@ export class AppService implements OnModuleInit {
 
   async onModuleInit() {
     // ✅ subscribe ไว้ล่วงหน้าเฉพาะ reply topic (เพราะเรารู้ชื่อแน่นอน)
-    this.kafka.subscribeToResponseOf(this.replyTopic);
+    console.log("Subscribing to requestTopic topic: ", this.requestTopic);
+    this.kafka.subscribeToResponseOf(this.requestTopic);
     await this.kafka.connect();
   }
 
 async testTransform(data: any): Promise<any> {
-  const correlationId = uuidv4();
+  console.log(`Sending request with correlationId: ${this.requestTopic}`);
 
-  const response$ = this.kafka.send(
+  const response$ = await firstValueFrom(this.kafka.send(
     this.requestTopic,
     {
-      value: {
-        correlationId,
-        payload: data,
-      },
-      headers: {
-        'reply-topic': this.replyTopic,
-        'correlation-id': correlationId,
-      },
+      test: 'transform',
     }
-  );
+  ));
 
-  return firstValueFrom(response$);
+  console.log("Response Observable: ", response$);
+  return response$;
 }
 }
+ 
